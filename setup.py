ab6619a2498216a200c5d603d3bc57b085425b0c
Jesse Min Zhang <jessez@stanford.edu>
2018-11-11 13:06:35 -0800

    preparing code for pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'truncated_normal',
+  packages = ['truncated_normal'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Package for running the TN test described in https://www.biorxiv.org/content/early/2018/11/05/463265',
+  author = 'Jesse Min Zhang',
+  author_email = 'jessez@stanford.edu',
+  url = 'https://github.com/jessemzhang/tn_test',
+  download_url = 'https://github.com/jessemzhang/tn_test/archive/0.1',
+  keywords = ['truncated-normal', 'differential-expression', 'single-cell', 'rna-seq', 'clustering', 'tn-test'], 
+  classifiers = [],
+)
